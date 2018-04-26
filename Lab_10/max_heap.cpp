4ed9722fb8ca1fe11a58ec97907b13c1cc719d67
Nikhil Kumar <31308077+nikhil9402@users.noreply.github.com>
2018-04-26 20:45:06 +0530

    Update max_heap.cpp

--- Lab_10/max_heap.cpp
+++ Lab_10/max_heap.cpp
@@ -7,8 +7,8 @@ int heap[100];				//declares a heap
 
 void insert(int key){			//inserts an element
 	heap[length] = key;
-	int i=length;
-	int p=(i-1)/2;
+	int i=length;			//new element will be added to last	
+	int p=(i-1)/2;			//its parent
 
 	while(heap[i]>=heap[p] && i!=0){	//a loop to swap the elements if it violates heap property
 
@@ -26,46 +26,42 @@ void insert(int key){			//inserts an element
 	length++;
 }
 
-void delete_root(){
-
-	int a = heap[0];             //copy the last element of the array in the root.
-	int b = heap[length-1];
-	heap[0] = b;
-	heap[length-1] = a;
-
-	length--;			//decreases the loop length by 1.
-	
-	int current_node = 0;
-	int i = current_node;
-
-	while(heap[current_node] < (heap[(2*i)+1] || heap[(2*i)+2])){ 
-		if(2*current_node+1 > length-1){		//no children
-			break;
-		}
-
-		else if(2*current_node+1 == length-1){		//one children
-			if(heap[current_node] < heap[2*current_node+1]){
-				a = heap[current_node];
-				b = heap[2*current_node+1];
-				heap[current_node] = b;
-				heap[2*current_node+1] = a;
-			}
-		}
-
-		else if(2*current_node+1 < length-1){ 		//two children
-			if(heap[current_node] < heap[2*current_node+1]){
-				
-			}
-		}	
-	}
-
+void swap(int &a, int &b){		//swaps the elements
+	int c = a;
+	int d = b;
+	a = d;
+	b = c;
+	return;
 
 }
+
+void remove_root(){				//removes the node
+	heap[0] = heap[length-1];		//changes the value of the root
+	length--;				//decreases the length by 1
+	int i = 0;
+	int l = 0;
+	int r = 0;
+	cout << endl;
+
+	for(int i = 0;i<length;i++){
+		l = (2*i)+1;			//left child
+		r = (2*i)+2;			//right child
+		if(heap[i] < heap[l]){		//if element with index i is less than its left child
+			swap(heap[i],heap[(2*i)+1]);	//swap with its left child
+		}
+		if(heap[i] < heap[r]){		//if element with index i is less than its right child
+			swap(heap[i],heap[(2*i)+2]);	//swap with its right child
+		}
+		
+	}
+}
+
+
 
 
 int main(){
 	
-	insert(30);
+	insert(30);			//inserts element
 	insert(40);
 	insert(20);
 	insert(25);
@@ -73,8 +69,14 @@ int main(){
 	insert(100);
 	insert(12);
 
+	for (int j=0;j<length;j++){		//prints the heap
+		cout << heap[j] << endl;
+	}
+	
+	remove_root();			//removes the root
 	for (int j=0;j<length;j++){
 		cout << heap[j] << endl;
 	}
+	
 	return 0;
 }
